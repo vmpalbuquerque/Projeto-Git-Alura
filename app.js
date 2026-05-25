0972f2ba5a005acaa006e55b3c8bf6b616d02d6e
Victor Martins Pereira de Albuquerque <valbuquerque@brq.com>
2026-05-25 08:35:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+console.log('Olá Mundo!');
+
+var userName = 'Victor Martins'
+
+document.getElementById('user-name').innerHTML = userName
+
+//VARIÁVEIS//
+
+//var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof jedi)
+
+//OPERADORES MATEMÁTICOS//
+
+//var n1 = 5
+//var n2 = 2.5
+
+//console.log (typeof n1)
+//console.log (typeof n2)
+
+//var total = n1 / n2
+//console.log(total)
+
+//OPERADORES DE COMPARAÇÃO//
+
+//var v1 = 7
+//var v2 = '7'
+
+//var resultado = v1 !== v2
+//console.log(resultado)
+
+//FUNÇÕES//
+
+//function soma (n1 , n2){
+
+    //console.log (n1 + n2)
+//}
+
+//soma (10,3)
+
+/* function boasVindas(nome){
+    alert(nome + ', seja bem vindo!')
+
+}
+
+boasVindas('Victor') */
+
+/* function soma (n1 , n2) {
+    return n1 + n2
+
+}
+
+var resultado = soma (10 , 3)
+console.log(resultado) */
+
+//CONTROLE DE FLUXOS//
